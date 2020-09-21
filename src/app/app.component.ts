76e202fd9e8f98a969c262d26c1b8b4aaae7dac0
Digamber Singh <digamber@positronx.io>
2020-09-21 16:55:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+// app.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,28 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-qr-code-app-example';
+
+  public qrCodeVal: string = null;
+  public level: "L" | "M" | "Q" | "H";
+  public width: number;
+
+  constructor() {
+    this.level = "L";
+    this.qrCodeVal = "QR code string value";
+    this.width = 200;
+  }
+
+  updateLevel(newValue: "L" | "M" | "Q" | "H") {
+    this.level = newValue;
+  }
+
+  updateQrInfo(newValue: string) {
+    this.qrCodeVal = newValue;
+  }
+
+  updateWidth(newValue: number) {
+    this.width = newValue;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { QRCodeModule } from 'angular2-qrcode';
+import { FormsModule } from '@angular/forms'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QRCodeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
